19efb60e1e24d18df01aee6eea91560fdf5af716
rhianna <38383814+colure@users.noreply.github.com>
2021-07-21 21:01:54 -0400

    Conversion of static HTML pages to dynamic JSON load with HBS

--- app_server/controllers/meals.js
+++ app_server/controllers/meals.js
@@ -1,8 +1,13 @@
+const fs = require('fs');
+const menu = JSON.parse(fs.readFileSync('./data/menu.json'));
+
+
+/*GET view*/
 const meals = (req, res) => {
     pageTitle = process.env.npm_package_description + ' - Meals';
-    res.render('meals', {title: pageTitle});
+    res.render('meals', {title: pageTitle, menu});
 };
 
 module.exports = {
     meals
-}
+} 
--- app_server/controllers/rooms.js
+++ app_server/controllers/rooms.js
@@ -1,6 +1,10 @@
+const fs = require('fs');
+const room = JSON.parse(fs.readFileSync('./data/rooms.json', 'utf8'));
+
+/*GET ROOMS DATA*/
 const rooms = (req, res) => {
     pageTitle = process.env.npm_package_description + ' - Rooms';
-    res.render('rooms', {title: pageTitle});
+    res.render('rooms', {title: pageTitle, room});
 };
 
 module.exports = {
