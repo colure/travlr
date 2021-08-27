7a79333eaf0f5a651484676ef19d641cbd7e61ad
rhianna <38383814+colure@users.noreply.github.com>
2021-08-27 11:07:42 -0400

    Update storage.ts

--- app_admin/src/app/storage.ts
+++ app_admin/src/app/storage.ts
@@ -1,5 +1,8 @@
+/*Allows to access local storage cache.*/
+
 import {InjectionToken} from '@angular/core';
+
 export const BROWSER_STORAGE = new InjectionToken<Storage>('Browser Storage', {
     providedIn: 'root',
-    factory: () => localStorage
+    factory: () => localStorage //setup for local storage
 });
